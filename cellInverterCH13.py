bdadbe9936f504b714655beb0c68f74f7cae06ac
pirbpi <accountstuff@icloud.com>
2020-02-26 10:56:08 -0500

    Commit from Atom

--- cellInverterCH13.py
+++ cellInverterCH13.py
@@ -5,13 +5,12 @@ import openpyxl
 
 wb = openpyxl.load_workbook('produceSales.xlsx')
 sheet = wb.active
-rowMatrix = [] # Create an empty list
+rowMatrix = []  # Create an empty list
 
-for row in range(sheet.max_row): # For 'row' in range 0 to the last row with data
-    rowMatrix.append([]) # Create an empty sublist inside the list
-    for cellObj in list(sheet.rows)[row]: # For each cell in the current row
-        rowMatrix[row].append(cellObj.value) # Add the cell to the sub-list
-
+for row in range(sheet.max_row):  # For 'row' in range 0 to the last row with data
+    rowMatrix.append([])  # Create an empty sublist inside the list
+    for cellObj in list(sheet.rows)[row]:  # For each cell in the current row
+        rowMatrix[row].append(cellObj.value)  # Add the cell to the sub-list
 
 
 print(rowMatrix)
